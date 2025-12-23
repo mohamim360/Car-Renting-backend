b9786462d80e1e83e41f09eb9824a619a3081519
mohamim360 <mohamim360@gmail.com>
2025-12-23 13:19:09 +0600

    Initialize TypeScript Express server with basic setup,

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+const app: Application = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to Express & TypeScript Server');
+});
+
+export default app;
